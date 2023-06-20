0847562f86eede949bbdff04b036e324db121159
davidingles <davidingleslopez@gmail.com>
2023-06-20 11:01:00 +0200

    todo bien

--- components/Dav.js
+++ components/Dav.js
@@ -6,35 +6,29 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function Dav(props) {
-  const { nodes, materials } = useGLTF("/ECO.gltf");
+  const { nodes, materials } = useGLTF("/3D SUBKIT PROINDECSA.gltf");
   return (
     <group {...props} dispose={null}>
-      <group
-        position={[0.48, 0, 0.48]}
-        rotation={[Math.PI / 2, 0, 0]}
-        scale={0}
-      >
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["_1L0021__Convertido__ARD-geom"].geometry}
-          material={materials._1L0021__Convertido__ARD_front}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["_1L0021__Convertido__ARD-geom_1"].geometry}
-          material={materials["back-dil"]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["_1L0021__Convertido__ARD-geom_2"].geometry}
-          material={materials._1L0021__Convertido__ARD_edge}
-        />
-      </group>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes["TQ_ARD-geom001"].geometry}
+        material={materials["TQ_ARD_front.001"]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes["TQ_ARD-geom001_1"].geometry}
+        material={materials["TQ_ARD_back.001"]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes["TQ_ARD-geom001_2"].geometry}
+        material={materials["TQ_ARD_edge.001"]}
+      />
     </group>
   );
 }
 
-useGLTF.preload("/ECO.gltf");
+useGLTF.preload("/3D SUBKIT PROINDECSA.gltf");
